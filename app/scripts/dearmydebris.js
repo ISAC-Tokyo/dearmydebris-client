ceae8395439fe20ad43bd07cdc6538392de1fcc8
osoken <osoken@gmail.com>
2013-04-21 06:24:36 +0900

    configured some parameters and added an empty css.

--- app/scripts/dearmydebris.js
+++ app/scripts/dearmydebris.js
@@ -8,7 +8,7 @@ dearMyDebris.debris = new Array(0);
 dearMyDebris.initialViewPoint = {latitude:35.66193375685752, longitude: 139.67768669128418};
 dearMyDebris.queryBase = "http://192.168.27.149:3000/api/v1/debris/index.json";
 dearMyDebris.imageDirectoryBaseURL = 'http://192.168.26.160:3000/assets/';
-//27.149:3000
+
 dearMyDebris.fetchDebris = function(query)
 {
   $.get(dearMyDebris.queryBase+query).done(function(ret)
--- app/scripts/dearmydebris2d.js
+++ app/scripts/dearmydebris2d.js
@@ -5,12 +5,16 @@ dearMyDebris.initDebris = function(features)
 {
   features.forEach(function(o)
   {
+    var iconOffset = new google.maps.Point(0	, 0);
+    var iconPosition = new google.maps.Point(0, 0);
+    var iconSize = new google.maps.Size(128, 128);
+    var icon = new google.maps.MarkerImage(""+dearMyDebris.imageDirectoryBaseURL + ((o.properties.category!=null)?((o.properties.category)+".png"):"DEB.png"), iconSize, iconPosition, iconOffset);
     var markerOpts =
     {
       position: new google.maps.LatLng(o.geometry.coordinates[1],o.geometry.coordinates[0]),
       map: dearMyDebris.map,
       title: o.properties.name,
-      icon: ""+dearMyDebris.imageDirectoryBaseURL + ((o.properties.category!=null)?((o.properties.category)+".png"):"DEB.png")
+      icon: icon
     };
     var marker = new google.maps.Marker(markerOpts);
     var infowindow = new google.maps.InfoWindow({content: dearMyDebris.getContentString(o)});
@@ -23,7 +27,7 @@ function handleLoadGoogleMap()
 {
   var mapOptions = {
     center: new google.maps.LatLng(dearMyDebris.initialViewPoint.latitude, dearMyDebris.initialViewPoint.longitude),
-    zoom: 17,
+    zoom: 12,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     mapTypeControl: false,
     disableDefaultUI: true
